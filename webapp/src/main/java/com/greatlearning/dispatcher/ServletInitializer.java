aab186893e2dc3d2a1a7331a731f1d3e0f9838d6
Sudarsan Sivakumar <127577672+SudarsanSivakumar@users.noreply.github.com>
2023-06-09 20:35:05 +0530

    Add files via upload

---
+++ webapp/src/main/java/com/greatlearning/dispatcher/ServletInitializer.java
@@ -0,0 +1,7 @@
+package com.greatlearning.dispatcher;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+}
